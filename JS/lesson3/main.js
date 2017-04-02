ace4cf4029737416d231371cdf84f8336de467be
Aleksey <ad.tsay@outlook.com>
2017-04-02 14:05:24 -0700

    lesson 3 done

---
+++ JS/lesson3/main.js
@@ -0,0 +1,24 @@
+// Простые типы
+var myNumber = 1234;
+var myString = "Some string";
+var myBool = true;
+var myNull = null;
+var myUndefined = undefined;
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBool);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+
+// Объектные типы
+var obj = {name:"aleksey"};
+var array = [1,2,3];
+var regexp = /w+/g;
+var func = function(){};
+
+console.log("");
+console.log(typeof obj);
+console.log(typeof array);
+console.log(typeof regexp);
+console.log(typeof func);
