51536178494dcde687eedcda9851095b905237ac
Aleksey <ad.tsay@outlook.com>
2017-04-02 16:34:48 -0700

    Операторы сравнения

---
+++ JS/lesson4/main.js
@@ -0,0 +1,50 @@
+// Все числа в JS вещественные
+// console.log(5);
+// console.log(0);
+// console.log(300000);
+// console.log(0xffcc);
+// console.log(0xf);
+// console.log(0345);
+// console.log(0348);
+
+// console.log(12.45);
+// console.log(.89);
+// console.log(1.45e2);
+
+var nObject = new Number(4000);
+var nNumber = 4000;
+
+console.log("");
+
+console.log(typeof nObject);
+console.log(typeof nNumber);
+
+console.log(nObject.toFixed(2));
+console.log(nNumber.toFixed(2));
+console.log(2 .toFixed(2));
+
+console.log("");
+
+console.log(2 > 10); // false
+console.log(2 < 10); // true
+console.log(10 <= 10); // true
+console.log(8 >= 10); // false
+console.log(10 === 10); // true Сравнение на равенство
+console.log(10 !== 10); // false Сравнение на неравенство
+console.log(10 == 10); // true Сравнение на равенство с приведением типов (плохо)
+console.log(10 != 10); // false Сравнение на неравенство с приведением типов (плохо)
+console.log(10 == "10"); // true Сравнение на равенство с приведением типов (плохо)
+console.log(10 != "10"); // false Сравнение на неравенство с приведением типов (плохо)
+
+console.log("");
+
+console.log(0 === false); // false
+console.log(0 == false); // true
+console.log(5 === true); // false
+console.log(5 == true); // false
+console.log(1 == true); // true
+
+console.log("");
+
+
+
